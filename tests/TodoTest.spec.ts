348c0e25752d1ded62ef01e0db7d834d7ef8797f
Kevin Bjørn <kbj@skov.dk>
2023-08-29 11:14:20 +0200

    fix playwright test with localhost app starting now

--- tests/TodoTest.spec.ts
+++ tests/TodoTest.spec.ts
@@ -1,12 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, defineConfig } from '@playwright/test';
+
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:1234/');
+  await page.goto('http://localhost:1234');
   await page.getByPlaceholder('Add a task').click();
   await page.getByPlaceholder('Add a task').fill('test');
   await page.getByRole('button', { name: 'Add' }).click();
   await page.getByPlaceholder('Add a task').click();
-  await page.getByPlaceholder('Add a task').fill('test 2');
+  await page.getByPlaceholder('Add a task').fill('test2');
   await page.getByRole('button', { name: 'Add' }).click();
   await page.getByRole('button').nth(2).click();
   await page.getByRole('button').nth(1).click();
